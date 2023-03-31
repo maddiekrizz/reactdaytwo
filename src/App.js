aee5932c460d23ea49c5175b7218a0a685d57b06
unknown <doctormaddie31@gmail.com>
2023-03-31 15:35:49 +0530

    hey day one two

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <h1>
+      Welcome to Maddie's server :)</h1>
+      <h5>click here and get knowing what happens when you click !!</h5>
+     <buton onClick={window['print']}>Click here</buton>
       </header>
     </div>
   );
